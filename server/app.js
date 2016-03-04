19d7d8fdc2cec603f055e140f9cd647a5288a9ac
bonestormiii <aaron.t.lockhart@gmail.com>
2016-03-03 22:01:01 -0700

    Copy/Delete files from/to backup directory when a file is marked/unmarked for backup

--- server/app.js
+++ server/app.js
@@ -8,36 +8,43 @@ var util = require('./util');
 
 var createCuratorApp = function (init) {
     init = init || {};
-    
+
     // load in config settings
     var configString = fs.readFileSync("./resource/config.txt");
     var configData = JSON.parse(configString);
 
     init.fileInfo.dir = configData["imgSrcDir"];
     var backupDir = configData["backupDir"] || './data';
-    
+
     var instance = {
         // fileInfo manages the metadata for all the files in the file set
         fileInfo: fileInfo(init.fileInfo),
-        
+
         // An instance of express used for routing server requests.
         expressInstance: express(),
-        
+
         // Moves the files marked for backup to the backup folder.
-        moveFilesToBackupFolder: function () {
-            var keepFiles = this.fileInfo.getFilteredMetadata(function (data) { return data.keep == true; });
-            
+        moveFilesToBackupFolder: function (files) {
+            var keepFiles = files || this.fileInfo.getFilteredMetadata(function (data) { return data.keep == true; });
+
             for (var i = 0; i < keepFiles.length; i++) {
                 var currentFilePath = keepFiles[i].getPath();
                 var backupFilePath = backupDir + keepFiles[i].getPartialPath();
-                
-                (function(src, dst, meta, app) {
-                    util.copyFile(currentFilePath, backupFilePath, function () {
-                        console.log("Done copying file " + meta.filename);
+
+                (function (source, dest, meta, app) {
+                    util.copyFile(source, dest, function () {
+                        console.log("Done copying file " + dest);
                         meta.backedUp = true;
+                        meta.backupPath = dest;
                         app.fileInfo.updateFileMetadata(meta.filename, meta);
                     });
                 })(currentFilePath, backupFilePath, keepFiles[i], this);
+            }
+        },
+
+        moveToBackupFolder: function (filename) {
+            if (filename) {
+                this.moveFilesToBackupFolder([this.fileInfo.getFileMetadata(filename)]);
             }
         },
     };
--- server/filemetadata.js
+++ server/filemetadata.js
@@ -1,26 +1,43 @@
 var pathModule = require('path');
+var util = require('./util');
 
 var createFileMetadata = function (init) {
     init = init || {};
-    
+
     return {
+        // The filename
         filename: init.filename || '',
+        
+        // The path to the file (not including filename)
         path: init.path || '',
+        
+        // A flag inidicating if this file is marked for backup
         keep: init.keep || false,
+        
+        // Tags for the image
         tags: init.tags || [],
+        
+        // The current index of the file
         index: init.index >= 0 ? init.index : -1,
+        
+        // A flag indicating if the file has been backed up
         backedUp: init.backedUp === undefined ? false : init.backedUp,
         
+        // The full path (including filename) of the backup file
+        backupPath: init.backupPath || '',
+        
         // Gets the full path to the file including filename
-        getPath: function() {
-            return this.path + pathModule.sep +  this.filename;
+        getPath: function () {
+            return this.path + pathModule.sep + this.filename;
         },
         
-        // Gets a partial path to the file which is the parent folder and filename
+        // Gets a partial path to the file which is the parent folder and filename optionally 
+        // appends the filename to the path.
+        //
         // The partial path includes a file separator character at the beginning of the path
-        getPartialPath: function(appendFileName) {
+        getPartialPath: function (appendFileName) {
             appendFileName = appendFileName === undefined ? true : appendFileName;
-            
+
             var parentIndex = this.path.lastIndexOf(pathModule.sep)
             var parent = this.path.substr(parentIndex);
             if (appendFileName) {
@@ -30,6 +47,35 @@ var createFileMetadata = function (init) {
                 return parent;
             }
         },
+
+        updateProperty: function (property, newValue) {
+            var previousValue = this[property];
+            this[property] = newValue;
+
+            if (previousValue !== newValue) {
+                if (this.metadataChanged) {
+                    this.metadataChanged(property, previousValue, newValue);
+                }
+            }
+        },
+
+        metadataChanged: function (property, previousValue, newValue) {
+            if (property === "keep" && newValue == false && previousValue == true && this.backedUp) {
+                
+                var onDelete = function (metadata) {
+                    return function (err) {
+                        if (err) {
+                            console.log("Keep status changed to false, but error occured trying to delete backup file. " + err);
+                        }
+                        console.log("Keep status changed to false, file deleted from backup");
+                        metadata.backedUp = false;
+                        metadata.backupPath = '';
+                    }
+                }(this);
+                
+                util.deleteFile(this.backupPath, onDelete);
+            }
+        }
     }
 }
 
